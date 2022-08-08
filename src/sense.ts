8bda160b9150673985f03d1da02e6c3578b936e4
TendoProject <81413625+TendoProject@users.noreply.github.com>
2022-08-08 23:13:09 +0700

    Add files via upload

--- src/sense.ts
+++ src/sense.ts
@@ -13,16 +13,6 @@ async function renderAsync(core: app.core.Core, sense: app.features.Sense) {
   await core.runAsync(() => {
     const players = core.entityList.value;
     const localPlayer = players.find(x => x.address === core.localPlayer.value);
-    if (localPlayer) {
-      sense.updateStates(localPlayer, players)
-      updateaimbot(localPlayer, players, aimbot)
-    };
+    if (localPlayer) sense.updateStates(localPlayer, players);
   });
-}
-
-function updateaimbot(localPlayer: app.core.Player | undefined, players: Array<app.core.Player>, aimbot: app.features.aimbot) {
- if (!localPlayer) return;
-  if (!location.hash.includes('enable-aimbot')) return;
-  aimbot.updateStates(levelName, localPlayer, players);
-}
 }
